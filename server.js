68f3b3d00617fdfd769f517a7cfbb6165ee5aa10
Maki Aboabida <maki-miko@hotmail.com>
2021-09-27 16:43:11 -0400

    updated responses

--- server.js
+++ server.js
@@ -42,16 +42,30 @@ filterByQuery = (query, animalsArray) => {
     return filteredResults
 }
 
+findById = (id, animalsArray) => {
+    const result = animalsArray.filter(animal => animal.id === id)[0]
+    return result
+  }
+
 app.get('/api/animals', (req, res) => {
-    let results = animals;
+    let results = animals
     // if the url has a query 
     if (req.query) {
         // filter it 
         results = filterByQuery(req.query, results)
     }
-    res.json(results);
+    res.json(results)
 });
 
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals)
+    if (result) {
+        res.json(result)
+    } else {
+        res.send(404)
+    }
+  })
+
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`)
 });
